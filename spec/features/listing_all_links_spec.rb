6d4023902d82600ef54132b678b903b961480f38
Makis Otman <makisotman@gmail.com>
2014-02-19 19:17:52 +0000

    fixed broken test due to footer content

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -5,8 +5,8 @@ feature 'User browses the list of links' do
   before(:each) {
     Link.create(
       :url => 'http://www.makersacademy.com',
-      :title => 'Makers Academy',
-      :tags => [Tag.first_or_create(:text => 'education')])
+      :title => 'Barca',
+      :tags => [Tag.first_or_create(:text => 'football')])
     Link.create(
       :url => 'http://www.google.com',
       :title => 'Google',
@@ -23,12 +23,12 @@ feature 'User browses the list of links' do
 
   scenario 'when opening the home page' do
     visit '/'
-    expect(page).to have_content('Makers Academy')
+    expect(page).to have_content('Barca')
   end
 
   scenario 'filtered by a tag' do
     visit 'tags/search'
-    expect(page).not_to have_content('Makers Academy')
+    expect(page).not_to have_content('Barca')
     expect(page).not_to have_content('Code.org')
     expect(page).to have_content('Google')
     expect(page).to have_content('Bing')
