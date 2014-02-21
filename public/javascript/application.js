c30b11a4b271a3fcda3bc2a72a087ad964a3be23
Makis Otman <makisotman@gmail.com>
2014-02-21 18:00:12 +0000

    added JS to show when link is favorited and unfavorited

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -6,13 +6,24 @@ function addFavouritesHandler() {
     var newOpacity = favourited ? 0 : 1;
     $(link).data('favourited', !favourited);
     $(this).animate({opacity: newOpacity});
+
+    showLinkFavouritedNotice(link);
   });
+}
+
+function showLinkFavouritedNotice (link) {
+  var favourited = !!$(link).data('favourited');
+  var name = $(link).find('.title').text();
+  var message = favourited ?
+                name + ' was added to favourites' :
+                name + ' was removed from favourites';
+  var $flash = $('<div></div>').addClass('flash-notice').html(message);
+  $flash.appendTo('#flash-container');
+  window.setTimeout(function(){
+    $($flash).fadeOut();
+  }, 1000);
 }
 
 $(function(){
   addFavouritesHandler();
-})
-
-$(function(){
-  console.log("Hello");
-})
+});
